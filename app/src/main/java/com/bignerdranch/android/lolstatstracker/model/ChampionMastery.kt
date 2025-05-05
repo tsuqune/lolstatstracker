0f961350d442deec309695d328ac60f4c7b8f4a9
revqueenge <Dealul02@gmail.com>
2025-05-06 00:09:59 +0700

    Класс мастерства чемпионов

---
+++ app/src/main/java/com/bignerdranch/android/lolstatstracker/model/ChampionMastery.kt
@@ -0,0 +1,8 @@
+package com.bignerdranch.android.lolstatstracker.model
+
+data class ChampionMastery(
+    val championId: Long,
+    val championLevel: Int,
+    val championPoints: Int,
+    val lastPlayTime: Long
+)
