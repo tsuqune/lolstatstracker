5f987d6b500203bb277ccc089b6b5905c86decc5
revqueenge <Dealul02@gmail.com>
2025-05-08 23:04:32 +0700

    Добавляем новые поля в мастерство чемпионов

--- app/src/main/java/com/bignerdranch/android/lolstatstracker/model/ChampionMastery.kt
+++ app/src/main/java/com/bignerdranch/android/lolstatstracker/model/ChampionMastery.kt
@@ -3,5 +3,6 @@ package com.bignerdranch.android.lolstatstracker.model
 data class ChampionMastery(
     val championId: Long,
     val championLevel: Int,
-    val championPoints: Int
+    val championPoints: Int,
+    var championName: String? = null
 )
