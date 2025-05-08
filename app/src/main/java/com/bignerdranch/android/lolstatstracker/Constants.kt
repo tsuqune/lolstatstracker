6961ec63beaed4b41e734f9b99f5f6d2fcb40ad9
revqueenge <Dealul02@gmail.com>
2025-05-08 23:04:18 +0700

    Обновляем константы

--- app/src/main/java/com/bignerdranch/android/lolstatstracker/Constants.kt
+++ app/src/main/java/com/bignerdranch/android/lolstatstracker/Constants.kt
@@ -9,7 +9,6 @@ object Constants {
     // Для Summoner и League API (региональный)
     const val REGIONAL_BASE_URL = "https://ru.api.riotgames.com/" // Для RU региона
 
-    const val DDRAGON_BASE_URL = "https://ddragon.leagueoflegends.com/cdn/"
-
-
+    const val DDRAGON_VERSION = "15.9.1" // Добавляем константу для версии
+    const val DDRAGON_BASE_URL = "https://ddragon.leagueoflegends.com/cdn/${DDRAGON_VERSION}/"
 }
