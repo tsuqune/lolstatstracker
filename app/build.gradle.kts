7542e2dc29d3102a1d7d433abb37a0d837b6d3d0
revqueenge <Dealul02@gmail.com>
2025-05-14 00:37:49 +0700

    удаление дублей

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,10 +50,6 @@ android {
 }
 
 dependencies {
-
-    // Coil для загрузки изображений
-    implementation("io.coil-kt:coil-compose:2.4.0")
-
 // Jetpack Compose
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.compose.material3:material3:1.1.2")
