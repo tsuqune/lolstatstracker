ffdc50749a53b0f80f78098d3c1edf4b5307d384
revqueenge <Dealul02@gmail.com>
2025-05-13 23:18:43 +0700

    Добавляем новые зависимости

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,14 @@ android {
 }
 
 dependencies {
+
+    // Coil для загрузки изображений
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
+// Jetpack Compose
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.compose.material3:material3:1.1.2")
+
     // Существующие зависимости
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
